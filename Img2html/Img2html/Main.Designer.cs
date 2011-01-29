16f2d2319dd53f7f4ba52edce8dbcc50b2b6eb31
fthmko <fthmko@gmail.com>
2011-01-29 21:20:22 +0800

    img2html simple only

---
+++ Img2html/Img2html/Main.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Img2html {
+    partial class Main {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbOption = new System.Windows.Forms.ComboBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.dlgFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.AllowDrop = true;
+            this.txtFolder.Location = new System.Drawing.Point(59, 6);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(239, 21);
+            this.txtFolder.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.txtFolder, "Double click to browse");
+            this.txtFolder.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtFolder_DragDrop);
+            this.txtFolder.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtFolder_DragEnter);
+            this.txtFolder.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.txtFolder_MouseDoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Option";
+            // 
+            // cmbOption
+            // 
+            this.cmbOption.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOption.FormattingEnabled = true;
+            this.cmbOption.Location = new System.Drawing.Point(59, 34);
+            this.cmbOption.Name = "cmbOption";
+            this.cmbOption.Size = new System.Drawing.Size(158, 20);
+            this.cmbOption.TabIndex = 4;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(223, 32);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "&Run!";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 100;
+            this.toolTip1.AutoPopDelay = 3000;
+            this.toolTip1.InitialDelay = 100;
+            this.toolTip1.ReshowDelay = 20;
+            // 
+            // dlgFolder
+            // 
+            this.dlgFolder.Description = "Please select the image folder.";
+            this.dlgFolder.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 63);
+            this.Controls.Add(this.cmbOption);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Img2html";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbOption;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.FolderBrowserDialog dlgFolder;
+    }
+}
+
---
+++ Img2html/Img2html/Main.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Img2html {
+    public partial class Main : Form {
+        Dictionary<String, String> db;
+
+        public Main() {
+            InitializeComponent();
+            this.Icon = global::Img2html.Properties.Resources.video_x_generic;
+            loadOption();
+        }
+
+        private void loadOption() {
+            db = new Dictionary<String, String>();
+            db.Add("Simple_html", global::Img2html.Properties.Resources.Simple_html);
+            db.Add("Simple_node", global::Img2html.Properties.Resources.Simple_node);
+            cmbOption.Items.Add("Simple");
+
+            //more!
+
+            cmbOption.SelectedIndex = 0;
+        }
+
+        private void txtFolder_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false)) {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void txtFolder_DragDrop(object sender, DragEventArgs e) {
+            try {
+                txtFolder.Text = ((String[])e.Data.GetData(DataFormats.FileDrop))[0];
+            } catch (System.Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void txtFolder_MouseDoubleClick(object sender, MouseEventArgs e) {
+            if (dlgFolder.ShowDialog() == DialogResult.OK) {
+                txtFolder.Text = dlgFolder.SelectedPath;
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e) {
+            if (!Directory.Exists(txtFolder.Text)) {
+                MessageBox.Show("Invalid folder path!");
+                return;
+            }
+
+            String html, content, node, title, path;
+            path = txtFolder.Text;
+            db.TryGetValue(cmbOption.Text + "_html", out html);
+            db.TryGetValue(cmbOption.Text + "_node", out node);
+            title = Path.GetFileName(path);
+            html = html.Replace("${title}", HttpUtility.HtmlEncode(title));
+            content = "";
+
+            String[] imgs = Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly);
+            foreach (String img in imgs) {
+                if (img.EndsWith(".bmp") || img.EndsWith(".jpg") || img.EndsWith(".png") || img.EndsWith(".gif")) {
+                    content += node.Replace("${path}", title + "/" + Path.GetFileName(img));
+                }
+            }
+            html = html.Replace("${content}", content);
+            path = Path.GetDirectoryName(path) + "\\" + title + ".html";
+            if (File.Exists(path)) File.Delete(path);
+            StreamWriter fl = File.CreateText(path);
+            fl.Write(html);
+            fl.Flush();
+            fl.Close();
+            MessageBox.Show("Done.");
+        }
+    }
+}
