42d7d3d609cf16623da74bcee6c79fe5408b2fd0
choso <fthmko@qq.com>
2011-08-16 14:59:20 +0800

    add NoSleep to repo

---
+++ NoSleep/NoSleep/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ZHANGZHENG419
+ * Date: 2011-07-20
+ * Time: 13:42
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace NoSleep
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.button1.Location = new System.Drawing.Point(12, 11);
+			this.button1.Name = "button1";
+			this.button1.Padding = new System.Windows.Forms.Padding(2, 0, 0, 0);
+			this.button1.Size = new System.Drawing.Size(24, 24);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "●";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(41, 11);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "12345";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(144, 46);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.KeyPreview = true;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "NoSleep";
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
---
+++ NoSleep/NoSleep/MainForm.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ZHANGZHENG419
+ * Date: 2011-07-20
+ * Time: 13:42
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace NoSleep
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		[DllImport("kernel32.dll")]
+		static extern uint SetThreadExecutionState(uint esFlags);
+		const uint ES_SYSTEM_REQUIRED = 0x00000001;
+		const uint ES_DISPLAY_REQUIRED = 0x00000002;
+		const uint ES_CONTINUOUS = 0x80000000;
+
+		int count = 0;
+		int max = 1000000;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			label1.Text = "" + count;
+		}
+		
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			this.TopMost = true;
+			if(count >= max){
+				count = 0;
+			} else {
+				count++;
+			}
+			label1.Text = "" + count;
+			this.Activate();
+			SendKeys.Send("1");
+			SetThreadExecutionState(ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED);
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			timer1.Enabled = !timer1.Enabled;
+			button1.Text = timer1.Enabled?"●":"○";
+		}
+	}
+}
